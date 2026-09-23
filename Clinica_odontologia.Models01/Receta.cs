aeebd687b4a74bc1b3dea3eea0b9c60ae2f9457b
Ingrid <geanellaingrid@gmail.com>
2026-09-23 16:56:48 -0500

    Agregue la clase Receta, de la tabla debil recetas

---
+++ Clinica_odontologia.Models01/Receta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinica_odontologia.Models01
+{
+    [Table("recetas")]
+    public class Receta
+    {
+        [Key]
+        [Column ("id_receta", TypeName = "serial")]
+        public int IdReceta { get; set; }
+
+        [Required]
+        [Column ("fecha_emision", TypeName = "timestamp without time zone")]
+        public DateTime Fecha_emision { get; set; }
+
+        [Required]
+        [Column("indicaciones", TypeName = "text")]
+        public string Indicaciones { get; set; }
+
+        //llave foranea
+        [ForeignKey("citas")]
+        [Column("id_cita")]
+        public int IdCita { get; set; }
+        public Citas? citas { get; set; }
+    }
+}
